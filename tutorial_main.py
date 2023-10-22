4f1f5dbc48876d635b9c93ce9bb305aedb31df7d
phoenixwilliams <32957699+phoenixwilliams@users.noreply.github.com>
2023-10-22 12:12:02 +0100

    update tutorial_main

--- tutorial_main.py
+++ tutorial_main.py
@@ -1,4 +1,6 @@
-import TAASSC_214_dev as lgr
+import TASSC_20058_stable as btr
+import TAASSC_215_dev as lgr
+
 import glob
 import argparse
 
@@ -6,15 +8,29 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Directory location of your text files.')
     parser.add_argument('--directory', type=str, help="Directory of your text files.")
     parser.add_argument('--filename', type=str, help="Name of the .csv file with your results.")
+    parser.add_argument('--dev', type=bool, help="Set to True if you want to use the version current in development",
+                        default=False)
     args = parser.parse_args()
 
     file_list = glob.glob("%s/*.txt" % args.directory)  # create your own filelist
-    lgr.LGR_Full(file_list, "%s.csv" % args.filename)
 
-    lgr.LGR_Full(args.directory, "%s.csv" % args.filename)  # let TAASSC generate the filelist based on a folder name
-    lgr.LGR_Full("%s/" % args.directory, "%s.csv" % args.filename,
-                 output=["xml"])  # generate summary count file, generate xml representation for each
-    lgr.LGR_Full("%s/" % args.directory, "%s.csv" % args.filename, output=["xml",
-                                                            "vertical"])  # generate summary count file, generate xml representation  and vertical representation for each
+    if not args.dev:
+
+        btr.BTR_Full(file_list, "%s.csv" % args.filename)
+        btr.BTR_Full(args.directory, "%s.csv" % args.filename)  # let TAASSC generate the filelist based on a folder name
+        btr.BTR_Full("%s/" % args.directory, "%s.csv" % args.filename,
+                     output=["xml"])  # generate summary count file, generate xml representation for each
+        btr.BTR_Full("%s/" % args.directory, "%s.csv" % args.filename, output=["xml",
+                                                                "vertical"])  # generate summary count file, generate xml representation  and vertical representation for each
+
+    else:
+        lgr.LGR_Full(file_list, "%s.csv" % args.filename)
+        lgr.LGR_Full(args.directory,
+                  "%s.csv" % args.filename)  # let TAASSC generate the filelist based on a folder name
+        lgr.LGR_Full("%s/" % args.directory, "%s.csv" % args.filename,
+                  output=["xml"])  # generate summary count file, generate xml representation for each
+        lgr.LGR_Full("%s/" % args.directory, "%s.csv" % args.filename, output=["xml",
+                                                                               "vertical"])  # generate summary count file, generate xml representation  and vertical representation for each
+
 
 
